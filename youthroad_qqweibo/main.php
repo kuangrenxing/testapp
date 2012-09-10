cbf81a396924b88d95e250289da3ef7d4a3d4a3a
Owen chen <chenxin448@126.com>
2012-09-10 22:16:20 +0800

    update ing main.php

--- youthroad_qqweibo/main.php
+++ youthroad_qqweibo/main.php
@@ -1,57 +1,96 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title></title>
-<link href="src/css/style.css" rel="stylesheet" type="text/css" />
-<script type="text/javascript"> 
-window.onload=function(){ 
-for(var ii=0; ii<document.links.length; ii++) document.links[ii].onfocus=function(){this.blur()} 
-} 
-</script>
-</head>
- 
-<body id="ztBodyBg">
-<div id="ztcontainer">
-    <div id="ztcontent">
-        <div class="BG1">
-        	<div class="head">
-            	<div class="head1 left">
-                	<a href="http://www.tuolar.com" class="logo"><img src="src/images/img_start_03.jpg"/></a>
-                    <img class="s1" src="src/images/img_start_06.jpg"/>
-                </div>
-                <div class="head2 ">
-                	<h1>重走青春路</h1>
-                    <img class="s2" src="src/images/img_start_09.jpg"/>
-                </div>
-            </div>
-            <div class="content">
-            	<div class="d1">
-                	<img class="m1" src="src/images/image_15.jpg"/>
-                    <div class="d2">
-                    	<p class="p1">输入你的微博昵称</p>
-                        <form ><input id="search1" name="" class="name" style=" background:url(src/images/start2_03.jpg) no-repeat; width:251px; height:53px; margin-top:15px; margin-bottom:25px; background-position:100% 100%;  padding:0; border:0; font-size:30px;" type="text"></form>
-                        <a style="margin-left:18px;" href="javascript:void(0);" onclick="location_url();" ><img src="src/images/image_18.jpg"/></a>
-                    </div>
-                </div>
-            </div>
-        </div>            
-  	</div>
-</div>
+<?php
+/**
+ * 此为PHP-SDK 2.0 的一个使用Demo,用于流程和接口调用演示
+ * 请根据自身需求和环境进相应的安全和兼容处理，勿直接用于生产环境
+ */
+error_reporting(0);
+require_once './Config.php';
+require_once './Tencent.php';
 
+OAuth::init($client_id, $client_secret);
+Tencent::$debug = $debug;
 
-<script language="javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js"></script>
-<script language="javascript">
-function location_url(){
-	var url = "http://testapp.dev/youthroad_qqconnect/";	
-	var txt = $('#search1').val();
-	
-	if(txt != ''){
-		window.location.href="http://www.tuolar.com/apps/qq/qq/?url="+url+"results.php&name="+txt;
-	}else{
-		alert('请您输入微博昵称!');
-	}
+//打开session
+session_start();
+header('Content-Type: text/html; charset=utf-8');
+
+if ($_SESSION['t_access_token'] || ($_SESSION['t_openid'] && $_SESSION['t_openkey'])) {//用户已授权
+   
+    //获取用户信息
+    $r = Tencent::api('user/info');
+    $userinfo = json_decode($r, true);
+    
+    //print_r($userinfo);
+    $_SESSION['userinfo'] = $userinfo['data'];
+    //print_r($_SESSION);
+    // 部分接口的调用示例
+    /**
+     * 发表图片微博
+     * pic参数后跟图片的路径,以表单形式上传的为 : $_FILES['pic']['tmp_name']
+     * 服务器目录下的文件为: dirname(__FILE__).'/logo.png'
+     * /
+    $params = array(
+        'content' => '测试发表一条图片微博'
+    );
+    $multi = array('pic' => dirname(__FILE__).'/logo.png');
+    $r = Tencent::api('t/add_pic', $params, 'POST', $multi);
+    echo $r;
+    
+    /**
+     * 发表图片微博
+     * 如果图片地址为网络上的一个可用链接
+     * 则使用add_pic_url接口
+     * /
+    $params = array(
+        'content' => '以链接形式发表一条图片微博',
+        'pic_url' => 'http://mat1.gtimg.com/www/iskin960/qqcomlogo.png'
+    );
+    $r = Tencent::api('t/add_pic_url', $params, 'POST');
+    echo $r;
+    */
+} else {//未授权
+    $callback = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];//回调url
+    if ($_GET['code']) {//已获得code
+        $code = $_GET['code'];
+        $openid = $_GET['openid'];
+        $openkey = $_GET['openkey'];
+        //获取授权token
+        $url = OAuth::getAccessToken($code, $callback);
+        $r = Http::request($url);
+        parse_str($r, $out);
+        //存储授权数据
+        if ($out['access_token']) {
+            $_SESSION['t_access_token'] = $out['access_token'];
+            $_SESSION['t_refresh_token'] = $out['refresh_token'];
+            $_SESSION['t_expire_in'] = $out['expire_in'];
+            $_SESSION['t_code'] = $code;
+            $_SESSION['t_openid'] = $openid;
+            $_SESSION['t_openkey'] = $openkey;
+            
+            //验证授权
+            $r = OAuth::checkOAuthValid();
+            if ($r) {
+                header('Location: ' . $callback);//刷新页面
+            } else {
+                exit('<h3>授权失败,请重试</h3>');
+            }
+        } else {
+            exit($r);
+        }
+    } else {//获取授权code
+        if ($_GET['openid'] && $_GET['openkey']){//应用频道
+            $_SESSION['t_openid'] = $_GET['openid'];
+            $_SESSION['t_openkey'] = $_GET['openkey'];
+            //验证授权
+            $r = OAuth::checkOAuthValid();
+            if ($r) {
+                header('Location: ' . $callback);//刷新页面
+            } else {
+                exit('<h3>授权失败,请重试</h3>');
+            }
+        } else{
+            $url = OAuth::getAuthorizeURL($callback);
+            header('Location: ' . $url);
+        }
+    }
 }
-</script>
-</body>
-</html>
