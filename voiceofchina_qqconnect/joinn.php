48b2272355bb6849e64781e7f9210be87d794c60
owen chen <chenxin448@126.com>
2012-09-13 15:33:47 +0800

    choice song

---
+++ voiceofchina_qqconnect/joinn.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title></title>
+<link href="src/css/style.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript"> 
+window.onload=function(){ 
+for(var ii=0; ii<document.links.length; ii++) document.links[ii].onfocus=function(){this.blur()} 
+} 
+</script>
+</head>
+ 
+<body id="ztBodyBg">
+<div id="ztcontainer">
+    <div id="ztcontent">
+        <div class="BG1">
+        	<div class="head">
+            	<div class="head1 left">
+                	<a href="http://www.tuolar.com" class="logo"><img title="拖拉网" src="src/images/img_start_03.jpg"/></a>
+                    <img title="" class="s1" src="src/images/img_start_06.jpg"/>
+                </div>
+                <div class="head2 ">
+                	<img src="src/images/img_08.jpg" title="中国好声音"/>
+                </div>
+            </div>
+            <div class="content_2">
+            	<div class="banner2">
+                	<div class="d3">
+                    	<div class="d4">
+                        	<img src="src/images/user_03.jpg"/>
+                            <p> ***中国好声音由凉茶领导者***冠名播出，***凉茶，正宗好凉茶，很可惜没有老师为你转身</p>
+                        </div>
+                        <div class="d5">
+                        	<a href="" target=""><img src="src/images/focus_03.jpg"/></a>
+                            <a href="" target=""><img src="src/images/signAgain_03.jpg"/></a>
+                        </div>
+                    </div> 
+                </div>
+            </div>
+        </div>            
+  	</div>
+  	<div class="ztfooter"><p>Copyright Tuolar.com All Rights Reserved</p></div>	
+</div>
+
+</body>
+</html>
---
+++ voiceofchina_qqconnect/joiny.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title></title>
+<link href="src/css/style.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript"> 
+window.onload=function(){ 
+for(var ii=0; ii<document.links.length; ii++) document.links[ii].onfocus=function(){this.blur()} 
+} 
+</script>
+</head>
+ 
+<body id="ztBodyBg">
+<div id="ztcontainer">
+    <div id="ztcontent">
+        <div class="BG1">
+        	<div class="head">
+            	<div class="head1 left">
+                	<a href="http://www.tuolar.com" class="logo"><img title="拖拉网" src="src/images/img_start_03.jpg"/></a>
+                    <img title="" class="s1" src="src/images/img_start_06.jpg"/>
+                </div>
+                <div class="head2 ">
+                	<img src="src/images/img_08.jpg" title="中国好声音"/>
+                </div>
+            </div>
+            <div class="content_2">
+            	<div class="banner2">
+                	<h1>你太强了！！有两位导师为你转身，请选择一位进入他的小组</h1>
+                    <div class="d1">
+                    	<div class="box">
+                        	<div class="d2"><img src="src/images/images_06.jpg"/>
+                            	 <a href="" target="_blank"><img src="src/images/images_11.jpg"/></a>		
+                            </div>
+                            <p>选我选我选我。。。</p>
+                        </div>
+                        <div class="box">
+                        	<div class="d2"><img src="src/images/images_03.jpg"/>
+                            	 <a href="" target="_blank"><img src="src/images/images_11.jpg"/></a>		
+                          </div>
+                            <p>我会让你更细腻，来我这儿吧！我的团队就缺一个你这样的，我的队里就缺你这一号的</p>
+                        </div>
+                        <div class="box">
+                        	<div class="d2"><img src="src/images/images_16.jpg"/>
+                            	 <a href="" target="_blank"><img src="src/images/images_11.jpg"/></a>		
+                          </div>
+                            <p>来我的团队吧，我会把你打造成一位全方位的歌手；明天1月1号，五棵松体育馆，我有一场超大型的跨年演唱会。</p>
+                        </div>
+                        <div class="box">
+                        	<div class="d2"><img src="src/images/images_14.jpg"/>
+                            	 <a href="" target="_blank"><img src="src/images/images_11.jpg"/></a>		
+                          </div>
+                            <p>我今年有32场演唱会。我是一位创作型歌手，你来我的团队我会专门为你写歌。</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>            
+  	</div>
+  	<div class="ztfooter"><p>Copyright Tuolar.com All Rights Reserved</p></div>	
+</div>
+
+</body>
+</html>
