d354193c0932dcce438b9354e3efd3762b083fd5
owen chen <chenxin448@126.com>
2012-09-13 13:56:58 +0800

    china good voice index ok

---
+++ voiceofchina_qqconnect/index.php
@@ -0,0 +1,50 @@
+<?php
+session_start();
+$_SESSION["SFZ"] = "3200";
+include_once 'common/define.php';
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title></title>
+<link href="src/css/style.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript"> 
+window.onload=function(){ 
+for(var ii=0; ii<document.links.length; ii++) document.links[ii].onfocus=function(){this.blur()} 
+} 
+</script>
+</head>
+ 
+<body id="ztBodyBg">
+<div id="ztcontainer">
+    <div id="ztcontent">
+        <div class="BG1">
+        	<div class="head">
+            	<div class="head1 left">
+                	<a href="http://www.tuolar.com" class="logo"><img title="拖拉网" src="src/images/img_start_03.jpg"/></a>
+                    <img title="" class="s1" src="src/images/img_start_06.jpg"/>
+                </div>
+                <div class="head2 ">
+                	<img src="src/images/img_08.jpg" title="中国好声音"/>
+                </div>
+            </div>
+            <div class="content">
+            	<a class="start" href="javascript:void(0);" onclick="location_url();" target="_blank"><img src="src/images/img_14.jpg"/></a>
+            </div>
+        </div>            
+  	</div>
+  	<div class="ztfooter"><p>Copyright Tuolar.com All Rights Reserved</p></div>	
+</div>
+<script language="javascript">
+function location_url(){
+	var url = "<?php echo BASEURL;?>";	
+	var qqurl = "http://www.tuolar.com/apps/qq/qq/";	
+	
+	window.location.href= qqurl+"?url="+url+"song.php";		
+	
+}
+</script>
+
+</body>
+</html>
---
+++ voiceofchina_qqconnect/song.php
@@ -0,0 +1,52 @@
+<?php 
+
+
+?>
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title></title>
+<link href="src/css/style.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript"> 
+window.onload=function(){ 
+for(var ii=0; ii<document.links.length; ii++) document.links[ii].onfocus=function(){this.blur()} 
+} 
+</script>
+</head>
+ 
+<body id="ztBodyBg">
+<div id="ztcontainer">
+    <div id="ztcontent">
+        <div class="BG1">
+        	<div class="head">
+            	<div class="head1 left">
+                	<a href="http://www.tuolar.com" class="logo"><img title="拖拉网" src="src/images/img_start_03.jpg"/></a>
+                    <img title="" class="s1" src="src/images/img_start_06.jpg"/>
+                </div>
+                <div class="head2 ">
+                	<img src="src/images/img_08.jpg" title="中国好声音"/>
+                </div>
+            </div>
+            <div class="content_2">
+            	<div class="banner">
+                	<ul>
+                    	<li><a href="" target=""><img src="src/images/choice_03.jpg" title="选择"/></a><p class="pp">A:我是不是你最疼爱的人</p></li>
+                        <li><a href="" target=""><img src="src/images/choice_03.jpg" title="选择"/></a><p class="pp">B:我是不是你最疼爱的人</p></li>
+                        <li><a href="" target=""><img src="src/images/choice_03.jpg" title="选择"/></a><p class="pp">C:我是不是你最疼爱的人</p></li>
+                        <li><a href="" target=""><img src="src/images/choice_03.jpg" title="选择"/></a><p class="pp">D:我是不是你最疼爱的人</p></li>
+                        <li><a href="" target=""><img src="src/images/choice_03.jpg" title="选择"/></a><p class="pp">E:我是不是你最疼爱的人</p></li>
+                        <li><a href="" target=""><img src="src/images/choice_03.jpg" title="选择"/></a><p class="pp">F:我是不是你最疼爱的人</p></li>
+                    
+                    </ul>
+                </div>
+            </div>
+        </div>            
+  	</div>
+  	<div class="ztfooter"><p>Copyright Tuolar.com All Rights Reserved</p></div>	
+</div>
+
+</body>
+</html>
